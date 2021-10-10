b4fd1f261c2d9b82f9b5c661751d9134893d6b4a
wasPrime <1135677507@qq.com>
2021-10-11 00:35:29 +0800

    fix string_split thoughtless bug which causes that empty strings at the beginning and end(if exist) of the input string won't enter the result vector. e.g.",a,b,c," -> ["a", "b", "c"], not correct ["", "a", "b", "c", ""]

--- src/utils/utils.cc
+++ src/utils/utils.cc
@@ -12,13 +12,14 @@ const std::vector<std::string> Utils::string_split(const std::string& str,
                                                    const std::string& delimiter) {
     std::vector<std::string> ret;
 
-    std::string::size_type last_pos = str.find_first_not_of(delimiter, 0);
-    std::string::size_type pos = str.find_first_of(delimiter, last_pos);
-    while (pos != std::string::npos || last_pos != std::string::npos) {
-        ret.push_back(str.substr(last_pos, pos - last_pos));
-        last_pos = str.find_first_not_of(delimiter, pos);
-        pos = str.find_first_of(delimiter, last_pos);
+    size_t last = 0;
+    size_t index = str.find_first_of(delimiter, last);
+    while (index != std::string::npos) {
+        ret.emplace_back(str.substr(last, index - last));
+        last = index + delimiter.size();
+        index = str.find_first_of(delimiter, last);
     }
+    ret.emplace_back(str.substr(last, index - last));
 
     return ret;
 }
